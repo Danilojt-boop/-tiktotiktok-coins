9c9ffb41bb42b88718b3348ea17afcf7f7f0d695
Danilojt-boop <macedodanilo043@gmail.com>
2025-01-20 00:20:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -160,3 +160,17 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
   
+document.addEventListener("DOMContentLoaded", () => {
+  const addLinkForm = document.getElementById("add-link-form");
+  const showAddLinkFormBtn = document.getElementById("show-add-link-form");
+
+  // Exibir ou ocultar o formulário de adicionar link
+  showAddLinkFormBtn.addEventListener("click", () => {
+    if (addLinkForm.classList.contains("hidden")) {
+      addLinkForm.classList.remove("hidden");
+    } else {
+      addLinkForm.classList.add("hidden");
+    }
+  });
+});
+  
